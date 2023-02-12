d7cde6e80d84599c3dd8afec0f1718f1ed2d08da
c0057166 <b.d.hall2@newcastle.ac.uk>
2023-02-12 11:30:33 +0000

    Created two main files with soe functions such as change_screen Added some styling to homescreen.kv

---
+++ main.py
@@ -0,0 +1,23 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+class HomeScreen(Screen):
+    pass
+class SettingsScreen(Screen):
+    pass
+
+
+GUI = Builder.load_file("main.kv")
+class MainApp(App):
+    def build(self):
+        return GUI
+
+    def change_screen(self, screen_name):
+        #Get the screen manager from kv file and switch screen
+        print(self.root.ids)
+        screen_manager = self.root.ids['screen_manager']
+        screen_manager.current = screen_name
+        screen_manager.transition.direction = 'left'
+
+MainApp().run()
